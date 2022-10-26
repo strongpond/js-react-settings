19b673e8747a979d1eeb7b54d22ac6c4cb4d5a94
euiyeonlee <dmldus1121@naver.com>
2022-10-26 09:28:05 +0900

    prettier settings

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 100, // printWidth default 80 => 100 으로 변경
+  singleQuote: false, // "" => ''
+  arrowParens: "avoid", // arrow function parameter가 하나일 경우 괄호 생략
+  semi: true,
+  tabWidth: 2,
+};
